3a996358b680e86ae9e00261587ef2b40144fee5
Sodiq Ganiyu <ganiyusodiq059@gmail.com>
2023-09-25 14:26:48 +0100

    dark mode authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from '@/components/mode-toggle';
 import { UserButton } from '@clerk/nextjs';
 
 const Home = () => {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 };
